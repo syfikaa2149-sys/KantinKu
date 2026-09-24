ddc7935b637a5f90ef9fb42466cd8307ee61cfaa
syfikaa2149-sys <email-syfikaa2149@gmail.com>
2026-09-24 09:29:31 +0800

    feat(modul-08): integrasi bootstrap 5 custom css dan js

---
+++ FrontEnd/Assets/js/main.js
@@ -0,0 +1,19 @@
+// Utility Functions KantinKu
+function formatRupiah(angka) {
+    return new Intl.NumberFormat('id-ID', { 
+        style: 'currency', 
+        currency: 'IDR', 
+        minimumFractionDigits: 0 
+    }).format(angka);
+}
+
+function escapeHtml(text) {
+    const map = { 
+        '&': '&amp;', 
+        '<': '&lt;', 
+        '>': '&gt;', 
+        '"': '&quot;', 
+        "'": '&#039;' 
+    };
+    return String(text).replace(/[&<>"']/g, m => map[m]);
+}
